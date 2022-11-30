30abb903f9449cf1d845284da77c1ba4333877b8
Eduardo Salavarria <eesalava@espol.edu.ec>
2022-11-30 02:37:35 -0500

    finalizacion de la primera tabla

--- proyecto02/js/script.js
+++ proyecto02/js/script.js
@@ -7,32 +7,48 @@ window.addEventListener("DOMContentLoaded", (event) => {
   //obtenerPreciosHistoricos("bitcoin");
 });
 
-let obtenerPrecioEnFecha = (fecha) => {
-  lista_post = [3.727, 3.818, 1.455, 6.917];
-  lista_simbolos = ["BTC", "ETHER", "ADA", "XML"];
-  /* ids.forEach((id) => {
-        fetch(`${API_URL}${id}/history?date=${fecha}&localization=false`)
-          .then((response) => response.json())
-          .then((cryptoinfo) => {
-            let posts = cryptoinfo["community_data"]["reddit_average_posts_48h"]
-            let simbolo = cryptoinfo["symbol"]
-            console.log(posts)
-            lista_post.push(posts)
-           
-          })
-          .catch((error) => {
-            console.log("An error has ocurred");
-          });
-      }); */
-  let sum = lista_post.reduce((previous, current) => (current += previous));
-  //let avg = sum / lista_post.length;
+let obtenerPrecioEnFecha = async (opcion) => {
+  //lista_post = [3.727, 3.818, 1.455, 6.917];
+  //lista_simbolos = ["BTC", "ETHER", "ADA", "XML"];
+  lista_post = [];
+  lista_simbolos = [];
+
+  ids.forEach((id) => {
+    fetch(`${API_URL}${id}/history?date=20-12-2020&localization=false`)
+      .then((response) => response.json())
+      .then((cryptoinfo) => {
+        let dato = cryptoinfo["community_data"][opcion];
+        console.log(dato);
+        let simbolo = cryptoinfo["symbol"];
+        lista_post.push(dato);
+        lista_simbolos.push(simbolo)
+      })
+      .catch((error) => {
+        console.log("An error has ocurred");
+      });
+  });
+  
+  //console.log([1,2,34])
+  function testAsync(){
+    return new Promise((resolve,reject)=>{
+        //here our function should be implemented 
+        setTimeout(()=>{
+            console.log("Hello from inside the testAsync function");
+            resolve();
+        ;} , 5000
+        );
+    });
+}
+await testAsync();
+
+  let sum =  lista_post.reduce((previous, current) => (current += previous));
   let chart = document.querySelector("#chart-content");
 
+  console.log(lista_simbolos)
   for (let i = 0; i < lista_post.length; i++) {
     let percentage = lista_post[i] / sum;
     console.log(percentage);
     chart.innerHTML += `<th scope="row"> ${lista_simbolos[i]}</th> <td style="--size:${percentage};"></td>`;
-    //`<tr><th scope="row">${lista_simbolos[i]}</th><td style="--size: ${percentage}" ></td></tr>`
   }
 };
 
@@ -53,36 +69,51 @@ let obtenerPreciosHistoricos = (coin) => {
             console.log("An error has ocurred");
           }); */
 
-  let precios_historicos = [0.19, 0.23, 0.28, 0.4, 0.36, 0.34, 0.46, 0.51, 0.44];
+  let precios_historicos = [
+    0.19, 0.23, 0.28, 0.4, 0.36, 0.34, 0.46, 0.51, 0.44,
+  ];
 
-  let chart_precio_historico = document.querySelector("#chart-content-price")
-  for (let i = 0; i < precios_historicos.length-1; i++) {
+  let chart_precio_historico = document.querySelector("#chart-content-price");
+  for (let i = 0; i < precios_historicos.length - 1; i++) {
     precio = precios_historicos[i];
     console.log(precio);
     chart_precio_historico.innerHTML += `<tr>
-    <td style="--start:${precios_historicos[i]};  --size: ${precios_historicos[i+1]} "> <span class="data"> ${precios_historicos[i+1]}</span> </td>
-  </tr>`
+    <td style="--start:${precios_historicos[i]};  --size: ${
+      precios_historicos[i + 1]
+    } "> <span class="data"> ${precios_historicos[i + 1]}</span> </td>
+  </tr>`;
   }
-
 };
 
-let btn_consultar_reddit = document.querySelector(".chart-comentario-btn-1")
-btn_consultar_reddit.addEventListener("click", () => {obtenerPrecioEnFecha("20-12-2020")});
+let btn_consultar_reddit = document.querySelector(".chart-comentario-btn-1");
+btn_consultar_reddit.addEventListener("click", () => {
+  let seleccion_reddit = document.querySelector("#seleccion_reddit").value;
 
+  if (seleccion_reddit == 1) {
+    obtenerPrecioEnFecha("reddit_average_comments_48h");
+  } else if (seleccion_reddit == 2) {
+    obtenerPrecioEnFecha("reddit_average_posts_48h");
+  }
+});
 
-let btn_clear_reddit = document.querySelector(".chart-comentario-btn-2")
+let btn_clear_reddit = document.querySelector(".chart-comentario-btn-2");
 btn_clear_reddit.addEventListener("click", () => {
   //se limpia la tabla
-  let tabla_reddit = document.querySelector("#chart-content")
-  tabla_reddit.innerHTML = ""
+  let tabla_reddit = document.querySelector("#chart-content");
+  tabla_reddit.innerHTML = "";
 });
 
-let btn_price_cryp = document.querySelector(".chart-price-btn-1")
-btn_price_cryp.addEventListener("click", () => {obtenerPreciosHistoricos("bitcoin")});
+let btn_price_cryp = document.querySelector(".chart-price-btn-1");
+btn_price_cryp.addEventListener("click", () => {
+  obtenerPreciosHistoricos("bitcoin");
+});
 
-let btn_clear_price = document.querySelector(".chart-price-btn-2")
+let btn_clear_price = document.querySelector(".chart-price-btn-2");
 btn_clear_price.addEventListener("click", () => {
   //se limpia la tabla
-  let tabla_reddit = document.querySelector("#chart-content-price")
-  tabla_reddit.innerHTML = ""
+  let tabla_reddit = document.querySelector("#chart-content-price");
+  tabla_reddit.innerHTML = "";
 });
+
+seleccion_reddit.options[Selection.selectedIndex];
+let seleccion_precio = document.getElementById("seleccion_precio");
